7bfd321db774cd713aea17d3942d277d70379579
vendlnotfoundedu <nagy.vendel.2023d@telekimezotur.hu>
2026-09-24 11:36:31 +0200

    3. fa

---
+++ 03_FeludefinialasEsListak/Jatekos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03_FeludefinialasEsListak
+{
+    internal class Jatekos
+    {
+        private int szint;
+        private int pontszam;
+        public string Nev {  get; set; }
+        public int Szint { 
+            get { return szint; }
+            set {
+                if (value < 1)
+                {
+                    szint = 1;
+                }
+                else if (value > 100)
+                {
+                    szint = 100; 
+                }
+                else
+                {
+                    szint = value;
+                } 
+            } 
+        }
+        public int Pontszam { 
+            get 
+            { return pontszam; }
+            set 
+            {
+                if (value < 0) pontszam = 0;
+                else pontszam = value;
+            }
+        }
+        public Jatekos(string nev, int szint,  int pontszam)
+        {
+            Nev = nev;
+            Szint=szint;
+            Pontszam=pontszam;
+        }
+        public override string ToString()
+        {
+            return $"Játékos: {Nev}, Szint: {Szint}, Pontszám: {Pontszam}";
+        }
+    }
+}
---
+++ 03_FeludefinialasEsListak/Program.cs
@@ -0,0 +1,8 @@
+﻿using _03_FeludefinialasEsListak;
+
+Jatekos Bela = new Jatekos("Béla", 5, 100);
+Console.WriteLine(Bela.ToString());
+//Console.WriteLine($"Játékos: {Bela.Nev}, Szint: {Bela.Szint}, Pontszám: {Bela.Pontszam}");
+Jatekos Eva = new Jatekos("Éva", 120, 200);
+Console.WriteLine(Eva.ToString());
+//Console.WriteLine($"Játékos: {Eva.Nev}, Szint: {Eva.Szint}, Pontszám: {Eva.Pontszam}");
