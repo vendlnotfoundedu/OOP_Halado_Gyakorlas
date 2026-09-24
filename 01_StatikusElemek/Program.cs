1016b3982a770353350e97de5b1accf9fe009156
vendlnotfoundedu <nagy.vendel.2023d@telekimezotur.hu>
2026-09-24 10:47:17 +0200

    1. fa

--- 01_StatikusElemek/Program.cs
+++ 01_StatikusElemek/Program.cs
@@ -1,1 +1,4 @@
 ﻿using _01_StatikusElemek;
+Console.WriteLine($"Az átváltott 100 EUR értéke Forintban: {ValutaValto.EurToHuf(100)}");
+Console.WriteLine($"Az átváltott 250 USD értéke Forintban: {ValutaValto.UsdToHuf(250)}");
+Console.WriteLine($"Az összes átváltás száma: {ValutaValto.GetOsszesAtvaltas()}");
