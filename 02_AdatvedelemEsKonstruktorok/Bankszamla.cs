6e34633541151b0855b46534a1abe73613034ad5
vendlnotfoundedu <nagy.vendel.2023d@telekimezotur.hu>
2026-09-25 10:44:48 +0200

    2. fa

---
+++ 02_AdatvedelemEsKonstruktorok/Bankszamla.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02_AdatvedelemEsKonstruktorok
+{
+    internal class Bankszamla
+    {
+        private string szamlaszam;
+        private double egyenleg;
+        private double hitelKeret;
+        public string Szamlaszam{ 
+            get { return szamlaszam; } }
+        public double Egyenleg { get { return egyenleg; } }
+        public double HitelKeret {
+            get { return hitelKeret; }
+            set
+            {
+                if (value < 0)
+                {
+                    hitelKeret = 0;
+                }
+                else
+                {
+                    hitelKeret = value;
+                }
+            }
+
+        } 
+        public Bankszamla(string szamlaszam)
+        {
+            this.szamlaszam = szamlaszam;
+            this.egyenleg = 0;
+            this.hitelKeret = 50000;
+        }
+        public Bankszamla(string szamlaszam, double induloEgyenleg, double hitelKeret):this(szamlaszam) {
+            this.egyenleg = induloEgyenleg;
+            this.hitelKeret = hitelKeret;
+        }
+        public void Befizetes(double osszeg)
+        {
+            if (osszeg > 0)
+            {
+                this.egyenleg += osszeg;
+            }
+        }
+        public bool Kivetel(double osszeg)
+        {
+            if (egyenleg-osszeg>= -(hitelKeret))
+            {
+                egyenleg -= osszeg;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ 02_AdatvedelemEsKonstruktorok/Program.cs
@@ -0,0 +1,7 @@
+﻿using _02_AdatvedelemEsKonstruktorok;
+Bankszamla Anna = new Bankszamla("87654321-87654321-87654321");
+Bankszamla Bela = new Bankszamla("12345678-12345678-12345678", 100000, 50000);
+Anna.Befizetes(10000);
+Bela.Kivetel(20000);
+Console.WriteLine($"Anna számlaszáma: {Anna.Szamlaszam}, egyenlege: {Anna.Egyenleg} Ft, hitelkerete: {Anna.HitelKeret} Ft");
+Console.WriteLine($"Béla számlaszáma: {Bela.Szamlaszam}, egyenlege: {Bela.Egyenleg} Ft, hitelkerete: {Bela.HitelKeret} Ft");
